3ce200c4129709af3dae26cd5644ab109a11f82d
orgwirelist123 <orgwirelist123@gmail.com>
2020-08-21 14:37:27 -0400

    it works

---
+++ App2.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import PushNotification from './components/PushNotification';
+console.log("app.js ran");
+// const AppNavigator = createStackNavigator(
+//   {
+//     PushNotification: {
+//       Screen: PushNotification,
+//       navigationOptions: {
+//         headerShown: false
+//       }
+//     }
+//   },
+//   {
+//     initialRouteName: 'PushNotification',
+//   }
+// );
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
