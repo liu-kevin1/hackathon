bbb3fa1f7dc21c43a5323c04814fdeaa20e73f9f
orgwirelist123 <orgwirelist123@gmail.com>
2020-08-21 13:11:54 -0400

    installed dependencies

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
 import PushNotification from './components/PushNotification';
 
 const AppNavigator = createStackNavigator(
